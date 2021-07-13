fbb47d4ee4861acae4e1714641635ce7250f949f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-07-13 16:47:22 -0600

    successfully updated package.lock to remove unncessary dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import _ from 'lodash';
+import './reset.css';
 import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = 'Hello world';
   element.classList.add('hello');
 
   return element;
