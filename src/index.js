1f889d5b6817de0a849c78b3ba7f9d74f8aaab35
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-07-13 17:49:31 -0600

    Create function that iterates through todo list and populates html to-do list container

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import './reset.css';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const toDoListDisplay = document.querySelector('.to-do-list');
 
-  element.innerHTML = 'Hello world';
-  element.classList.add('hello');
-
-  return element;
+const task1 = {
+  description: 'wash car',
+  completed: false,
+  index: 0,
 }
 
-document.body.appendChild(component());
+const task2 = {
+  description: 'take fluffy to the vet',
+  completed: false,
+  index: 1,
+}
+
+const task3 = {
+  description: 'finish regexp tutorial',
+  completed: false,
+  index: 2,
+}
+
+const TO_DO_LIST_TASKS = [task1, task2, task3];
+
+const addTasktoDisplay = (task) => {
+  toDoListDisplay.innerHTML += `<li class="to-do-list__item" id="${task.index}">${task.description}</li>`;
+}
+
+TO_DO_LIST_TASKS.forEach(task => addTasktoDisplay(task));
+
